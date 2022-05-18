c309d61bde711def364a5de970c40ee7eb87b2e1
Maciej Matula <maciek.matula@gmail.com>
2022-05-17 20:20:04 -0500

    commit tree bb85122aa49cae86946281047cb07794f5fd95ad

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  if(x <= 400){
+    return 'This one is on me!'
+  }else if(x < 2000){
+    return 'That will be twenty bucks.'
+  }else if (x <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(x){
   // Write your code here!
+  const message = x === "NYC" ? 'Ok, sounds good.' : "No go."
+  return message
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(x){
   // Write your code here!
+  let message;
+
+  switch(x){
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = "Thank you.";
+      break;
+    default:
+      message = "Bye.";
+      break;
+  }
+  return message
 }
